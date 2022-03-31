d105bd23bc5379c8cd60d3c1367749a93355d158
a1104004650 <1104004650@qq.com>
2022-03-31 17:12:55 +0800

    积分服务

---
+++ point-server/src/main/java/com/zbc/point/PointserverApplication.java
@@ -0,0 +1,13 @@
+package com.zbc.point;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointserverApplication.class, args);
+    }
+
+}
