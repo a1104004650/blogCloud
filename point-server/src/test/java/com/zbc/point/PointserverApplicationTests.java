67d3cf82399ccbc5f3cc5e5d4a06b839e6a5131d
a1104004650 <1104004650@qq.com>
2022-03-31 17:13:27 +0800

    积分服务

---
+++ point-server/src/test/java/com/zbc/point/PointserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zbc.point;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
